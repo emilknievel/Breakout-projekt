c1add1a1077c50d10b7976af2a405c8d6d3d24cc
emilknievel <emil.vagstedt@gmail.com>
2013-03-19 16:54:59 +0100

    Cleaned up a to-do

--- src/liu/ida/emiva760/tddc69/projekt/GameBoard.java
+++ src/liu/ida/emiva760/tddc69/projekt/GameBoard.java
@@ -255,7 +255,7 @@ public class GameBoard extends JPanel implements SharedConstants {
                         score += 200;
                     }
 
-                    //TODO: I can probably place the powerup making methods around here (use rands)
+
                     if (bricks[i].getType() != 1 && randomSpawnPower < 6) {
                         createPowerUp(bricks[i].getX(), bricks[i].getY(), powerType);
                         localPowerType = powerType;
