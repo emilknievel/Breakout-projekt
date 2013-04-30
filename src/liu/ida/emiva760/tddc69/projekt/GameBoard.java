6eaef5cb27bf5de4a4d4a0c455ff58a8229e1a34
emilknievel <emil.vagstedt@gmail.com>
2013-04-30 12:30:26 +0200

    Made the brick array 2D instead.

--- src/liu/ida/emiva760/tddc69/projekt/GameBoard.java
+++ src/liu/ida/emiva760/tddc69/projekt/GameBoard.java
@@ -13,7 +13,7 @@ public class GameBoard extends JPanel implements SharedConstants {
 	String message = "Game Over! ";
 	Ball ball;
 	Paddle paddle;
-	Brick bricks[];
+    Brick bricks[][];
     PowerUp powerUp;
 	
 	boolean gameRunning = true;
@@ -37,8 +37,8 @@ public class GameBoard extends JPanel implements SharedConstants {
 	public GameBoard() {
 		addKeyListener(new TAdapter());
 		setFocusable(true);
-		
-		bricks = new Brick[30];
+
+        bricks = new Brick[5][6];
 		setDoubleBuffered(true);
 		gameTimer = new Timer();
 		gameTimer.scheduleAtFixedRate(new ScheduleTask(), 1000, 10);
@@ -52,14 +52,14 @@ public class GameBoard extends JPanel implements SharedConstants {
 	public void gameInit() {
 		ball = new Ball();
 		paddle = new Paddle();
-		
-		int brickIndex = 0;
-		
+
+        // Place the blocks on 5 rows and 6 columns
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 6; j++) {
                 blockType = randomNo.nextInt(3);
-				bricks[brickIndex] = new Brick(j*50, i*30+50, blockType);
-				brickIndex++;
+				//bricks[brickIndex] = new Brick(j*50, i*30+50, blockType);
+                bricks[i][j] = new Brick(j*50, i*30+50, blockType);
+				//brickIndex++;
 			}
 		}
 	}
@@ -84,18 +84,21 @@ public class GameBoard extends JPanel implements SharedConstants {
 			g2.drawImage(paddle.getImage(), paddle.getX(), paddle.getY(),
 					paddle.getWidth(), paddle.getHeight(), this);
 
-			for (int i = 0; i < 30; i++) {
-				if (!bricks[i].isDestroyed()) {
-                    g2.drawImage(bricks[i].getImage(), bricks[i].getX(),
-                            bricks[i].getY(), bricks[i].getWidth(),
-                            bricks[i].getHeight(), this);
-                } else {
-                    if (bricks[i].getType() != 1 && powerUp != null) {
-                        g2.drawImage(powerUp.getImage(), powerUp.getX(), powerUp.getY(),
-                                powerUp.getWidth(), powerUp.getHeight(), this);
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 6; j++) {
+                    if (!bricks[i][j].isDestroyed()) {
+                        g2.drawImage(bricks[i][j].getImage(), bricks[i][j].getX(),
+                                bricks[i][j].getY(), bricks[i][j].getWidth(),
+                                bricks[i][j].getHeight(), this);
+                    } else {
+                        if (bricks[i][j].getType() != 1 && powerUp != null) {
+                            g2.drawImage(powerUp.getImage(), powerUp.getX(), powerUp.getY(),
+                                    powerUp.getWidth(), powerUp.getHeight(), this);
+                        }
                     }
                 }
-			}
+
+            }
 		} else {
 			Font font = new Font("Sans", Font.BOLD, 11);
 			FontMetrics metrics = this.getFontMetrics(font);
@@ -156,17 +159,20 @@ public class GameBoard extends JPanel implements SharedConstants {
                 livesString = "Lives: " + Integer.toString(lives);
             }
 		}
-		
-		for (int i = 0, j = 0; i < 30; i++) {
-			if (bricks[i].isDestroyed()) {
-				j++;
-			}
-			
-			if (j == 30) {
-				message = "You win! ";
-				stopGame();
-			}
-		}
+
+        int destroyedBlocksCounter = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (bricks[i][j].isDestroyed()) {
+                    destroyedBlocksCounter++;
+                }
+
+                if (destroyedBlocksCounter == 30) {
+                    message = "You win! ";
+                    stopGame();
+                }
+            }
+        }
 		
 		if ((ball.getRect()).intersects(paddle.getRect())) {
 
@@ -203,65 +209,87 @@ public class GameBoard extends JPanel implements SharedConstants {
 				ball.setYDir(-1);
 			}
 		}
-		
-		for (int i = 0; i < 30; i++) {
-			if ((ball.getRect()).intersects(bricks[i].getRect())) {
 
-				int ballLeft = (int)ball.getRect().getMinX();
-				int ballHeight = (int)ball.getRect().getHeight();
-				int ballWidth = (int)ball.getRect().getWidth();
-				int ballTop = (int)ball.getRect().getMinY();
+		for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                if ((ball.getRect()).intersects(bricks[i][j].getRect())) {
 
-				Point pointRight =
-						new Point(ballLeft + ballWidth + 1, ballTop);
-				Point pointLeft = new Point(ballLeft - 1, ballTop);
-				Point pointTop = new Point(ballLeft, ballTop - 1);
-				Point pointBottom =
-						new Point(ballLeft, ballTop + ballHeight + 1);
+                    int ballLeft = (int)ball.getRect().getMinX();
+                    int ballHeight = (int)ball.getRect().getHeight();
+                    int ballWidth = (int)ball.getRect().getWidth();
+                    int ballTop = (int)ball.getRect().getMinY();
 
-				if (!bricks[i].isDestroyed()) {
-					if (bricks[i].getRect().contains(pointRight)) {
-						ball.setXDir(-1);
-					}
+                    Point pointRight =
+                            new Point(ballLeft + ballWidth + 1, ballTop);
+                    Point pointLeft = new Point(ballLeft - 1, ballTop);
+                    Point pointTop = new Point(ballLeft, ballTop - 1);
+                    Point pointBottom =
+                            new Point(ballLeft, ballTop + ballHeight + 1);
 
-					else if (bricks[i].getRect().contains(pointLeft)) {
-						ball.setXDir(1);
-					}
+                    if (!bricks[i][j].isDestroyed()) {
+                        if (bricks[i][j].getRect().contains(pointRight)) {
+                            ball.setXDir(-1);
+                        }
 
-					if (bricks[i].getRect().contains(pointTop)) {
-						ball.setYDir(1);
-					}
+                        else if (bricks[i][j].getRect().contains(pointLeft)) {
+                            ball.setXDir(1);
+                        }
 
-					else if (bricks[i].getRect().contains(pointBottom)) {
-						ball.setYDir(-1);
-					}
+                        if (bricks[i][j].getRect().contains(pointTop)) {
+                            ball.setYDir(1);
+                        }
 
-					bricks[i].setDestroyed(true);
+                        else if (bricks[i][j].getRect().contains(pointBottom)) {
+                            ball.setYDir(-1);
+                        }
 
-                    if (bricks[i].getType() == 0) {
-                        score += 100;
+                        bricks[i][j].setDestroyed(true);
+
+                        if (bricks[i][j].getType() == 0) {
+                            score += 100;
+                        }
+
+                        //TODO: Fix so that it gives the points only when destroyed
+                        //Currently it gives points the strike before and when it's destroyed
+                        else if (bricks[i][j].getType() == 1 && bricks[i][j].getHealth() == 0) {
+                            score += 50;
+                        }
+                        else if (bricks[i][j].getType() == 2) {
+                            //TODO: Add the explosive effect here. Think about points
+
+                            // New stuff
+                            // ============================================================
+                        /*
+                        If j == 0, don't check to the left of the block
+                        If j == 5, don't check to the right of the block
+
+                        If i == 0, don't check above the block
+                        If i == 4, don't check below the block
+
+                        Assuming everything above is correct:
+                            Do the check:
+                                If i == 0 && j == 0:
+                                    check a[i+1][j] (the one below)
+                                    check a[i][j+1] (the one to the right)
+                        */
+                            // ============================================================
+
+                            score += 200;
+                        }
+
+
+                        if (bricks[i][j].getType() != 1 && randomSpawnPower < 4) {
+                            createPowerUp(bricks[i][j].getX(), bricks[i][j].getY(), powerType);
+                            localPowerType = powerType;
+                        }
+
+                        scoreString = "Score: " + Integer.toString(score);
                     }
+                }
+            }
 
-                    //TODO: Fix so that it gives the points only when destroyed
-                    //Currently it gives points the strike before and when it's destroyed
-                    else if (bricks[i].getType() == 1 && bricks[i].getHealth() == 0) {
-                        score += 50;
-                    }
-                    else if (bricks[i].getType() == 2) {
-                        //TODO: Add the explosive effect here. Think about points
-                        score += 200;
-                    }
-
-
-                    if (bricks[i].getType() != 1 && randomSpawnPower < 4) {
-                        createPowerUp(bricks[i].getX(), bricks[i].getY(), powerType);
-                        localPowerType = powerType;
-                    }
-
-					scoreString = "Score: " + Integer.toString(score);
-				}
-			}
 		}
+
 	}
 
     private void useExtraLife() {
@@ -320,19 +348,19 @@ public class GameBoard extends JPanel implements SharedConstants {
     }
 
     private boolean blockAbove() {
-        return
+        return bricks[i-1][j] yadda yadda
     }
 
     private boolean blockBelow() {
-
+        return bricks[i+1][j]
     }
 
     private boolean blockLeft() {
-
+        return bricks[i][j-1]
     }
 
     private boolean blockRight() {
-
+        return bricks[i][j+1]
     }
     */
 }
