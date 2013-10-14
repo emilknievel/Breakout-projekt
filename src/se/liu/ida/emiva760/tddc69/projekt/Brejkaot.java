e207a14800fbb204d5f24252a66297358205bcc6
emilknievel <emil.vagstedt@gmail.com>
2013-10-14 17:38:28 +0200

    Comment for last commit:
    Added javadocs and cleaned up the code a bit.

--- src/se/liu/ida/emiva760/tddc69/projekt/Brejkaot.java
+++ src/se/liu/ida/emiva760/tddc69/projekt/Brejkaot.java
@@ -13,6 +13,7 @@ public class Brejkaot extends JFrame {
         setIgnoreRepaint(true);
         setResizable(false);
         setVisible(true);
+
     }
 
     public static void main(String[] args) {
